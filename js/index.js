5f66667f21bdfc6f1a29cba18357b296a99fff0c
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-02-03 10:18:03 +0100

    semantic and accessibility fixed

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 $(".hamburger-icon").on("click", function () {
+    $(this).toggleClass("active")
+    if($(this).hasClass("active")){
+        $(this).attr("aria-expanded", "true")
+    }
+    else{
+        $(this).attr("aria-expanded", "false")
+    }
     $(".line").each(function () {
         $(this).toggleClass("menu-clicked")
     })
